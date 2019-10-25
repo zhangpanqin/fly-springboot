3c3d37a49069549e1b7a9461877c075de8a5e8cf
张攀钦 <zhangpanqin>
2019-10-25 18:11:27 +0800

    feat:添加新的工具类学习

--- spring-interface/src/main/java/com/fly/spring/util/ResourceUtilsStudy.java
+++ spring-interface/src/main/java/com/fly/spring/util/ResourceUtilsStudy.java
@@ -1,5 +1,12 @@
 package com.fly.spring.util;
 
+import cn.hutool.core.io.IoUtil;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
+
 /**
  * @author 张攀钦
  * @date 2019-10-25-17:22
@@ -11,5 +18,39 @@ package com.fly.spring.util;
  * * @see org.springframework.core.io.ResourceLoader
  */
 public class ResourceUtilsStudy {
+    @Test
+    public void run() throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("demo/application.properties");
+        InputStream inputStream = classPathResource.getInputStream();
+        String read = IoUtil.read(inputStream, "UTF-8");
+        System.out.println(read);
+    }
 
+    @Test
+    public void run3() throws IOException {
+        ClassPathResource urlResource = new ClassPathResource("demo/application.txt");
+        InputStream inputStream = urlResource.getInputStream();
+        String read = IoUtil.read(inputStream, "UTF-8");
+        System.out.println(read);
+    }
+
+    /**
+     * 获取 classpath 开头
+     */
+    @Test
+    public void getFile() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:demo/application.txt");
+        String read = IoUtil.read(new FileInputStream(file), "UTF-8");
+        System.out.println(read);
+    }
+
+    /**
+     * 获取 file 开头
+     */
+    @Test
+    public void getFile2() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:demo/application.txt");
+        String read = IoUtil.read(new FileInputStream(file), "UTF-8");
+        System.out.println(read);
+    }
 }
--- spring-interface/src/test/java/com/fly/spring/SpringInterfaceApplicationTests.java
+++ spring-interface/src/test/java/com/fly/spring/SpringInterfaceApplicationTests.java
@@ -2,15 +2,20 @@ package com.fly.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringInterfaceApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
     @Test
     public void contextLoads() {
+        System.out.println(applicationContext);
     }
 
 }
