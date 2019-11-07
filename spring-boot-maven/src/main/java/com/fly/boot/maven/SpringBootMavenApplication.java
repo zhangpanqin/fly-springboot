ea1145c62e5df438942c2dad127eab4e39eaf9cf
万古云霄 <zhangpanqin@outlook.com>
2019-11-07 21:50:07 +0800

    feat：增加研究springboot 插件

---
+++ spring-boot-maven/src/main/java/com/fly/boot/maven/SpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.fly.boot.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenApplication.class, args);
+    }
+
+}
