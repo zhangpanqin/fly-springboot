c723ee3ca10720af85632f750c8f4682ef7d7443
张攀钦 <zhangpanqin>
2019-11-29 19:29:20 +0800

    feat:异步编程返回值

--- druid-demo/src/test/java/com/fly/druid/DruidDemoApplicationTests.java
+++ druid-demo/src/test/java/com/fly/druid/DruidDemoApplicationTests.java
@@ -2,13 +2,15 @@ package com.fly.druid;
 
 import com.fly.druid.druid.DepotDao;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 public class DruidDemoApplicationTests {
 
     @Autowired
@@ -34,5 +36,14 @@ public class DruidDemoApplicationTests {
             a++;
         }
     }
+
+    @Test
+    public void run33() {
+        Path path = Paths.get("E:\\");
+        File file = path.toFile();
+        String[] list = file.list();
+        System.out.println(Stream.of(list).collect(Collectors.joining()));
+    }
+
 
 }
