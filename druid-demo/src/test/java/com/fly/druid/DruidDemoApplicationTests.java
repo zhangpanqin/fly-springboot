428c977d86d55067fa465c1c623f281c4a3ae1dd
万古云霄 <zhangpanqin@outlook.com>
2019-12-11 01:04:25 +0800

    feat:添加 druid 链接配置

--- druid-demo/src/test/java/com/fly/druid/DruidDemoApplicationTests.java
+++ druid-demo/src/test/java/com/fly/druid/DruidDemoApplicationTests.java
@@ -2,15 +2,13 @@ package com.fly.druid;
 
 import com.fly.druid.druid.DepotDao;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DruidDemoApplicationTests {
 
     @Autowired
@@ -18,32 +16,10 @@ public class DruidDemoApplicationTests {
 
     @Test
     public void contextLoads() throws InterruptedException {
-        int a = 0;
-
-        if (a <= 500) {
-            new Thread(() -> {
-                System.out.printf("", depotDao.allDepots(1));
-            }).start();
-            a++;
-        }
-
-        Thread.sleep(1000);
-        a = 0;
-        if (a <= 500) {
-            new Thread(() -> {
-                System.out.printf("", depotDao.allDepots(1));
-            }).start();
-            a++;
-        }
+        System.out.println(depotDao.allDepots(1));
     }
 
-    @Test
-    public void run33() {
-        Path path = Paths.get("E:\\");
-        File file = path.toFile();
-        String[] list = file.list();
-        System.out.println(Stream.of(list).collect(Collectors.joining()));
-    }
+
 
 
 }
