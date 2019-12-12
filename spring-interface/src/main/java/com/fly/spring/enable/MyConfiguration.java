ba1405e64e970aff2c673381b1d4e64cb39e4362
张攀钦 <zhangpanqin>
2019-12-12 09:18:17 +0800

    feat:验证跨域传参

---
+++ spring-interface/src/main/java/com/fly/spring/enable/MyConfiguration.java
@@ -0,0 +1,14 @@
+package com.fly.spring.enable;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 张攀钦
+ * @date 2019-12-11-19:03
+ * @description
+ */
+@Configuration
+public class MyConfiguration {
+
+
+}
