8cf1535f70f87e8d9267dbd95e2676cee13b9e0b
张攀钦 <zhangpanqin>
2019-12-14 18:48:00 +0800

    feat:log4j2 研究

--- exception-study/src/main/java/com/fly/exception/ExceptionStudyApplication.java
+++ exception-study/src/main/java/com/fly/exception/ExceptionStudyApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExceptionStudyApplication {
 
     public static void main(String[] args) {
+        System.setProperty("ceshi", "ceshi34153145");
         SpringApplication.run(ExceptionStudyApplication.class, args);
     }
 
---
+++ exception-study/src/test/java/com/fly/exception/ConfigXmlTest.java
@@ -0,0 +1,16 @@
+package com.fly.exception;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 张攀钦
+ * @date 2019-12-13-16:10
+ * @description
+ */
+public class ConfigXmlTest {
+    @Test
+    public void run1() {
+
+    }
+
+}
